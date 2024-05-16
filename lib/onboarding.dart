69173b24c5f1a0ff2b4b3541afda7c9db13e6567
nataliaamah <ameranatalia@gmail.com>
2024-05-16 23:03:54 +0800

    second onboarding

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Onboarding extends StatelessWidget {
   final VoidCallback onCompleted;
@@ -10,9 +11,11 @@ class Onboarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Onboarding')),
         body: OnboardingScreen(onCompleted: onCompleted),
       ),
+      theme : ThemeData(
+          scaffoldBackgroundColor: Color.fromARGB(255, 10, 38, 39),
+        )
     );
   }
 }
@@ -45,6 +48,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> with TickerProvider
     super.dispose();
   }
 
+// build pageview
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -53,8 +57,9 @@ class _OnboardingScreenState extends State<OnboardingScreen> with TickerProvider
         PageView(
           controller: _pageViewController,
           onPageChanged: _handlePageViewChanged,
-          children: <Widget>[
+          children: [
             buildOnboardingOne(context),
+            Text('Testing'),
             // Add other onboarding pages here
           ],
         ),
@@ -80,12 +85,72 @@ class _OnboardingScreenState extends State<OnboardingScreen> with TickerProvider
   }
 
   bool _isOnDesktopAndWeb() {
-    // Your implementation here
-    return false;
+    if (kIsWeb) return true;
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.macOS:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        return true;
+      default:
+        return false;
+    }
   }
 
+// build page 1
   Widget buildOnboardingOne(BuildContext context) {
-    // Your implementation here
-    return Container();
+    return SafeArea(
+    child: Container(
+      width: double.maxFinite,
+      padding: EdgeInsets.symmetric(horizontal: 51, vertical: 92),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spacer(flex: 32),
+          Image.asset('assets/images/1.png', height: 300, width: 400),
+          Text(
+            "Your Guardian in Times of Need",
+            style: TextStyle(color: Colors.white, fontFamily: 'Roboto', fontWeight: FontWeight.w400,),
+          ),
+          Spacer(flex: 37),
+          SizedBox(height: 30),
+          SizedBox(
+            height: 40,
+            child: AnimatedSmoothIndicator(
+              activeIndex: 0,
+              count: 2,
+              effect: ScrollingDotsEffect(
+                activeDotColor: Color(0X1212121D),
+                dotHeight: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+  }
+
+  Widget buildOnboardingTwo(BuildContext context){
+    return SafeArea(
+      child: Container(
+        width: double.maxFinite,
+        padding: EdgeInsets.symmetric(horizontal: 51, vertical: 92),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            Text(
+              "Share Your Location",
+              style: TextStyle(color: Colors.white, fontFamily: 'Roboto', fontWeight: FontWeight.w400,),
+            ),
+            SizedBox(height : 30),
+            Image.asset('assets/images/1.png', height : 300, width: 300,),
+            
+          ]
+        )
+      ),
+      );
   }
 }
+
+
