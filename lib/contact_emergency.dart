79ef70b802723c81f879ad764fca3cb10f759b9f
nataliaamah <ameranatalia@gmail.com>
2024-06-24 00:10:17 +0800

    emergency fix

--- lib/contact_emergency.dart
+++ lib/contact_emergency.dart
@@ -3,8 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hawa_v1/contact_emergency_view.dart';
 import 'package:hawa_v1/home_page.dart';
 import 'package:flutter_vibrate/flutter_vibrate.dart';
-import 'dart:async';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:async';
 
 class ContactEmergencyPage extends StatefulWidget {
   final String phoneNumber;
@@ -62,6 +62,11 @@ class _ContactEmergencyPageState extends State<ContactEmergencyPage> {
     return phoneNumber;
   }
 
+  Future<String?> _fetchUserName(String userId) async {
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    return userDoc.exists ? userDoc['fullName'] : 'Unknown User';
+  }
+
   void _startVibrating() {
     _isVibrating = true;
     _vibrate();
@@ -78,8 +83,8 @@ class _ContactEmergencyPageState extends State<ContactEmergencyPage> {
   }
 
   String _formatTimestamp(Timestamp timestamp) {
-    DateTime date = timestamp.toDate();
-    return "${date.hour}:${date.minute}  |  ${date.day}/${date.month}/${date.year}";
+    final date = timestamp.toDate();
+    return '${date.hour}:${date.minute.toString().padLeft(2, '0')} | ${date.day}/${date.month}/${date.year}';
   }
 
   @override
@@ -140,9 +145,9 @@ class _ContactEmergencyPageState extends State<ContactEmergencyPage> {
                 ),
               ),
               textAlign: TextAlign.center,
+              ),
+              ),
             ),
-            ),
-          ),
           SizedBox(height: 30,),
           Expanded(
             child: StreamBuilder<QuerySnapshot>(
@@ -181,55 +186,57 @@ class _ContactEmergencyPageState extends State<ContactEmergencyPage> {
                     bool isResolved = data['resolved'] ?? true;
                     bool isNew = !isResolved && _isVibrating;
 
-                    return GestureDetector(
-                      onTap: () {
-                        if (isNew) {
-                          _stopVibrating();
-                        }
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ContactEmergencyViewPage(emergencyData: emergencyData),
+                    return FutureBuilder<String?>(
+                      future: _fetchUserName(data['userId']),
+                      builder: (context, snapshot) {
+                        String userName = snapshot.data ?? 'Unknown User';
+                        return GestureDetector(
+                          onTap: () {
+                            if (isNew) {
+                              _stopVibrating();
+                            }
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ContactEmergencyViewPage(emergencyData: emergencyData),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: isResolved ? Colors.transparent : Colors.yellow.withOpacity(0.6),
+                                  spreadRadius: 10,
+                                  blurRadius: 10,
+                                ),
+                              ],
+                            ),
+                            child: ListTile(
+                              title: Text(
+                                '$userName Needs Help!',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              subtitle: Text(
+                                'Emergency at ${_formatTimestamp(data['timestamp'])}',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              trailing: Icon(
+                                isResolved ? Icons.check_circle : Icons.warning,
+                                color: isResolved ? Colors.green : Colors.red,
+                              ),
+                            ),
                           ),
-                        ).then((_) {
-                          setState(() {}); // Refresh the state to update the card's appearance
-                        });
+                        );
                       },
-                      child: Container(
-                        margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(8.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: isResolved
-                                  ? Colors.transparent
-                                  : Colors.yellow.withOpacity(0.6),
-                              spreadRadius: 10,
-                              blurRadius: 10,
-                            ),
-                          ],
-                        ),
-                        child: ListTile(
-                          title: Text(
-                            "${widget.fullName} Needs Help!",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          subtitle: Text(
-                            'Emergency at ${_formatTimestamp(data['timestamp'])}',
-                            style: TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                          trailing: Icon(
-                            isResolved ? Icons.check_circle : Icons.warning,
-                            color: isResolved ? Colors.green : Colors.red,
-                          ),
-                        ),
-                      ),
                     );
                   },
                 );
